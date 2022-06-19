bf9fcd3bfffce26a380e7df2d3d76d57e3ea628e
mrsaeeddev <saeed_dev@yahoo.com>
2022-06-19 11:40:40 +0500

    implement create endpoint for article

--- main.go
+++ main.go
@@ -3,14 +3,30 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
+// function returing all articles
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles 🚀")
+	json.NewEncoder(w).Encode((Articles))
+}
+
+// function returning single article by ID
 func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Working")
 	vars := mux.Vars(r)
 	key := vars["id"]
 
@@ -21,9 +37,13 @@ func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllArticles 🚀")
-	json.NewEncoder(w).Encode((Articles))
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	var post Article
+	json.Unmarshal(reqBody, &post)
+	Articles = append(Articles, post)
+	json.NewEncoder(w).Encode(post)
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -36,6 +56,8 @@ func handleRequests() {
 
 	router.HandleFunc("/", homePage)
 	router.HandleFunc("/articles", returnAllArticles)
+	router.HandleFunc("/article", createNewArticle).Methods("POST")
+
 	router.HandleFunc("/article/{id}", returnSingleArticle)
 	log.Fatal(http.ListenAndServe((":10000"), router))
 }
@@ -58,12 +80,3 @@ func main() {
 	}
 	handleRequests()
 }
-
-type Article struct {
-	Id      string `json:"Id"`
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-
-var Articles []Article
